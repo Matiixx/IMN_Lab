c67f7c4025c7a1f048624a3f61cd388474a311e1
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-10-21 18:06:36 +0200

    RK2 Method

--- Lab1/plot.sh
+++ Lab1/plot.sh
@@ -1,6 +1,6 @@
 set term jpeg
 
-set xlabel "Kolejne iteracje" 
+set xlabel "t" 
 set ylabel "Wartosci" 
 
 set title "Metoda jawna Eulera"
@@ -10,3 +10,11 @@ plot "euler0.01.csv" u 1:2 w l lw 1 t 'dt = 0.01s', "euler0.10.csv" u 1:2 w l lw
 set title "Metoda jawna Eulera - blad"
 set out "../euler_diff.jpeg"
 plot "euler0.01_diff.csv" u 1:2 w l lw 1 t 'dt = 0.01s', "euler0.10_diff.csv" u 1:2 w l lw 1 t 'dt = 0.1s', "euler1.00_diff.csv" u 1:2 w l lw 1 t 'dt = 1.0s'
+
+set title "Metoda jawna RK2"
+set out "../rk2.jpeg"
+plot "rk20.01.csv" u 1:2 w l lw 1 t 'dt = 0.01s', "rk20.10.csv" u 1:2 w l lw 1 t 'dt = 0.1s', "rk21.00.csv" u 1:2 w l lw 1 t 'dt = 1.0s', "rk2_analytic.csv" u 1:2 w l lw 2 t 'analytic'
+
+set title "Metoda jawna RK2 - blad"
+set out "../rk2_diff.jpeg"
+plot "rk20.01_diff.csv" u 1:2 w l lw 1 t 'dt = 0.01s', "rk20.10_diff.csv" u 1:2 w l lw 1 t 'dt = 0.1s', "rk21.00_diff.csv" u 1:2 w l lw 1 t 'dt = 1.0s'
--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -26,15 +26,17 @@ double analticValue(double l, double t)
   return std::exp(l * t);
 }
 
-void eulerSchema()
+void eulerMethod()
 {
   double y0 = 1.0;
   double lambda = -1.0;
   double t_start = 0.0;
   double t_end = 5.0;
-  double time_steps[] = {0.01, 0.1, 1.0};
+  double time_stamps[] = {0.01, 0.1, 1.0};
 
-  for (const auto &dt : time_steps)
+  double res{};
+
+  for (const auto &dt : time_stamps)
   {
     std::stringstream stream;
     stream << std::fixed << std::setprecision(2) << dt;
@@ -45,30 +47,75 @@ void eulerSchema()
 
     int n = (t_end - t_start) / dt;
 
-    std::vector<double> y;
+    std::vector<double> y{};
     y.push_back(1.0);
 
     for (int i = 0; i < n; i++)
     {
-      double res = y[i] + dt * lambda * y[i];
+      res = y[i] + dt * lambda * y[i];
       y.push_back(res);
       saveToFile(filename, static_cast<double>(i) * dt, res);
 
       saveToFile(filenameDiffrence, static_cast<double>(i) * dt, res - analticValue(static_cast<double>(i) * dt, lambda));
     }
   }
+
   clearFile("euler_analytic.csv");
   double dt = 0.01;
   int n = (t_end - t_start) / dt;
-
   for (int i = 0; i < n; i++)
   {
     saveToFile("euler_analytic.csv", static_cast<double>(i) * dt, analticValue(static_cast<double>(i) * dt, lambda));
   }
 }
 
+void rk2Method()
+{
+
+  double y0 = 1.0;
+  double lambda = -1.0;
+  double t_start = 0.0;
+  double t_end = 5.0;
+  double time_stamps[] = {0.01, 0.1, 1.0};
+
+  for (const auto &dt : time_stamps)
+  {
+    std::stringstream stream;
+    stream << std::fixed << std::setprecision(2) << dt;
+    std::string filename = "rk2" + stream.str() + ".csv";
+    clearFile(filename);
+
+    std::string filenameDiffrence = "rk2" + stream.str() + "_diff.csv";
+    clearFile(filenameDiffrence);
+
+    std::vector<double> y{};
+    y.push_back(1.0);
+    int n = (t_end - t_start) / dt;
+    double res{}, k1{}, k2{};
+    for (int i = 0; i < n; i++)
+    {
+      k1 = lambda * y[i];
+      k2 = lambda * (y[i] + dt * k1);
+      res = y[i] + dt / 2.0 * (k1 + k2);
+
+      y.push_back(res);
+      saveToFile(filename, static_cast<double>(i) * dt, res);
+
+      saveToFile(filenameDiffrence, static_cast<double>(i) * dt, res - analticValue(static_cast<double>(i) * dt, lambda));
+    }
+  }
+  double dt = 0.01;
+  int n = (t_end - t_start) / dt;
+  clearFile("rk2_analytic.csv");
+  for (int i = 0; i < n; i++)
+  {
+    saveToFile("rk2_analytic.csv", static_cast<double>(i) * dt, analticValue(static_cast<double>(i) * dt, lambda));
+  }
+}
+
 int main()
 {
-  eulerSchema();
+  eulerMethod();
+  rk2Method();
   return 0;
 }
