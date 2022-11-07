e05e63a6ec7772919e3f6026a051da994da255c1
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-07 18:07:10 +0100

    Apply rk2 method and solve problem

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <math.h>
+#include <sstream>
 
 void clearFile(const std::string &f)
 {
@@ -19,11 +20,11 @@ void saveToFile(const std::string &f, const double &a, const double &b)
   file.close();
 }
 
-void saveToFile(const std::string &f, const double &a, const double &b, const double &c)
+void saveToFile(const std::string &f, const double &a, const double &b, const double &c, const double &d)
 {
   std::fstream file;
   file.open(f, std::ios::app);
-  file << a << " " << b << " " << c << "\n";
+  file << a << " " << b << " " << c << " " << d << "\n";
   file.close();
 }
 
@@ -37,6 +38,14 @@ const double alfa = 5;
 
 inline double f(const double &a) { return a; }
 inline double g(const double &a, const double &b) { return alfa * (1 - a * a) * b - a; }
+
+template <typename T>
+T max(const T &a, const T &b)
+{
+  if (a > b)
+    return a;
+  return b;
+}
 
 result metodaTrapezow(const double &xn, const double &vn, const double &dt, const double &alfa)
 {
@@ -68,9 +77,79 @@ result metodaTrapezow(const double &xn, const double &vn, const double &dt, cons
   return {xn1, vn1};
 }
 
+result metodaRK2(const double &xn, const double &vn, const double &dt, const double &alfa)
+{
+  double k1x = f(vn);
+  double k1v = g(xn, vn);
+
+  double k2x = f(vn + dt * k1v);
+  double k2v = g(xn + dt * k1x, vn + dt * k1v);
+
+  return {xn + dt / 2.0 * (k1x + k2x), vn + dt / 2.0 * (k1v + k2v)};
+}
+
+struct method
+{
+  result (*fun)(const double &, const double &, const double &, const double &);
+  std::string name;
+  result operator()(const double &xn, const double &vn, const double &dt, const double &alfa)
+  {
+    return fun(xn, vn, dt, alfa);
+  }
+};
+
+method methodArray[] = {
+    {metodaTrapezow, "metoda_trapezow"},
+    {metodaRK2, "metoda_rk2"}};
+
+void kkc(method schemat_numeryczny, const double &tol)
+{
+  double t = 0,
+         x0 = 0.01,
+         v0 = 0,
+         dt0 = 1,
+         S = 0.75,
+         tmax = 40,
+         p = 2.0,
+         xn = x0,
+         vn = v0,
+         dt = dt0,
+         Ex{},
+         Ev{};
+
+  std::stringstream ss;
+  ss << schemat_numeryczny.name << "_" << tol << ".dat";
+
+  const std::string filename = ss.str();
+
+  clearFile(filename);
+  do
+  {
+    auto [x2n1, v2n1] = schemat_numeryczny(xn, vn, dt, alfa);
+    auto [x2n2, v2n2] = schemat_numeryczny(x2n1, v2n1, dt, alfa);
+
+    auto [x1n2, v1n2] = schemat_numeryczny(xn, vn, 2 * dt, alfa);
+
+    Ex = (x2n2 - x1n2) / (pow(2, p) - 1);
+    Ev = (v2n2 - v1n2) / (pow(2, p) - 1);
+
+    if (max(fabs(Ex), fabs(Ev)) < tol)
+    {
+      t += 2 * dt;
+      xn = x2n2;
+      vn = v2n2;
+      saveToFile(filename, t, dt, xn, vn);
+    }
+    dt *= pow((S * tol) / max(fabs(Ex), fabs(Ev)), 1 / (p + 1));
+
+  } while (t < tmax);
+}
+
 int main()
 {
-  auto [x, y] = metodaTrapezow(1, 2, 3, 12);
-  std::cout << x << " " << y << "\n";
+  kkc(methodArray[0], TOL[0]);
+  kkc(methodArray[0], TOL[1]);
+  kkc(methodArray[1], TOL[0]);
+  kkc(methodArray[1], TOL[1]);
   return 0;
 }
