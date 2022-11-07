4e9291c964b8a9794755c7d2cde6a9f4c9f469c3
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-07 16:46:53 +0100

    Set multilplot

--- Lab2/plot.sh
+++ Lab2/plot.sh
@@ -1,16 +1,18 @@
-set term jpeg
+set term jpeg size 1080,1920
 
 set xlabel "t"
 set yrange [0:500]
 
+set out "./res.jpeg"
+set multiplot layout 3,1
+
 set title "Metoda Picarda"
-set out "picard.jpeg"
 plot "picard.dat" u 1:2 w l lw 2 t 'u(t)', "picard.dat" u 1:3 w l lw 2 t 'z(t)=N-u(t)'
 
 set title "Metoda Newtona"
-set out "newton.jpeg"
 plot "newton.dat" u 1:2 w l lw 2 t 'u(t)', "newton.dat" u 1:3 w l lw 2 t 'z(t)=N-u(t)'
 
 set title "Niejawna metoda RK2"
-set out "rk2.jpeg"
 plot "rk2.dat" u 1:2 w l lw 2 t 'u(t)', "rk2.dat" u 1:3 w l lw 2 t 'z(t)=N-u(t)'
+
+unset multiplot
