5bdd21298848245fcf981be5d467e67ed1351027
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-17 18:08:16 +0100

    Update plotting results

--- Lab4/plot.sh
+++ Lab4/plot.sh
@@ -1,35 +1,37 @@
-#S
-set term jpeg size 600,600
+set term jpeg size 1600,2100
 
-set out "./S_global.jpeg"
-# set multiplot layout 1,2
+#S(it) global
+set out "./results.jpeg"
+set multiplot layout 3,2
+set logscale x 10
 
-set title "S(it)"
-plot "global_0.6.dat" u 1:2 w l lw 2 t '0.6', "global_1.dat" u 1:2 w l lw 2 t '1.0'
+set title "Relaksacja globalna"
+set ylabel "S"
+set xlabel "nr iteracji"
+plot "global_0.6.dat" u 1:2 w l lw 2 t "w=0.6", "global_1.dat" u 1:2 w l lw 2 t 'w=1.0'
 
-# unset multiplot
+#S(it) local
+set title "Relaksacja lokalna"
+plot "local_s_1.dat" u 1:2 w l lw 2 t 'w=1.0', "local_s_1.4.dat" u 1:2 w l lw 2 t 'w=1.4', "local_s_1.8.dat" u 1:2 w l lw 2 t 'w=1.8', "local_s_1.9.dat" u 1:2 w l lw 2 t 'w=1.9'
+
+unset logscale x
+set ylabel 'y'
+set xlabel 'x'
 
 # V
-set term jpeg size 600,600
-set out "V_0.6.jpeg"
 set view map
-
+set title "Relaksacja globalna w=0.6"
+set xrange [0:15]
 splot "global_v_0.6.dat" u 1:2:3 w p pt 5 ps 1 palette
 
+set title "Relaksacja globalna w=1.0"
+splot "global_v_1.dat" u 1:2:3 w p pt 5 ps 1 palette
 
-# set out "./rk2.jpeg"
-# set multiplot layout 1,4
+# Error
+set title "Bład rozwiązania w=0.6"
+splot "global_v_0.6_error.dat" u 1:2:3 w p pt 5 ps 1 palette
 
-# set title "v(t)"
-# plot "metoda_rk2_1e-05.dat" u 1:4 w l lw 2 t 'tol=10^-^5', "metoda_rk2_0.01.dat" u 1:4 w l lw 2 t 'tol=10^-^2'
+set title "Bład rozwiązania w=1.0"
+splot "global_v_1_error.dat" u 1:2:3 w p pt 5 ps 1 palette
 
-# set title "x(t)"
-# plot  "metoda_rk2_1e-05.dat" u 1:3 w l lw 2 t 'tol=10^-^5', "metoda_rk2_0.01.dat" u 1:3 w l lw 2 t 'tol=10^-^2'
-
-# set title "dt(t)"
-# plot "metoda_rk2_1e-05.dat" u 1:2 w l lw 2 t 'tol=10^-^5', "metoda_rk2_0.01.dat" u 1:2 w l lw 2 t 'tol=10^-^2'
-
-# set title "v(x)"
-# plot "metoda_rk2_1e-05.dat" u 3:4 w l lw 2 t 'tol=10^-^5', "metoda_rk2_0.01.dat" u 3:4 w l lw 2 t 'tol=10^-^2'
-
-# unset multiplot
+unset multiplot
