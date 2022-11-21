03244e19931dc429b42ae6d431c8c7b58de1eea1
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-21 17:09:57 +0100

    Update plot.sh

--- Lab5/plot.sh
+++ Lab5/plot.sh
@@ -4,7 +4,6 @@ set term jpeg size 1800,1200
 set out "./results.jpeg"
 set multiplot layout 2,3
 
-set title "k=16"
 # set ylabel "y"
 # set xlabel "x"
 # set view map
@@ -13,42 +12,29 @@ set title "k=16"
 set xlabel "x"
 set ylabel "y"
 set zlabel "v"
-# set xrange [0:22.4]
-# set yrange [0:22.4] 
-# set logscale z
+set xrange [0:22.4]
+set yrange [0:22.4] 
+# # set logscale z
 
+set title "k=16"
 plot [-3:29][-3:29] "mulitmesh_V_16.dat" u 1:2:3 w p pt 5 ps 8 palette notitle
+set title "k=8"
 plot [-3:29][-3:29] "mulitmesh_V_8.dat" u 1:2:3 w p pt 5 ps 4 palette notitle
+set title "k=4"
 plot [-3:29][-3:29] "mulitmesh_V_4.dat" u 1:2:3 w p pt 5 ps 2 palette notitle
+set title "k=2"
 plot [-3:29][-3:29] "mulitmesh_V_2.dat" u 1:2:3 w p pt 5 ps 1 palette notitle
+set title "k=1"
 plot [-3:29][-3:29] "mulitmesh_V_1.dat" u 1:2:3 w p pt 5 ps 0.5 palette notitle
 
+# unset view map
 
 #S(it)
 set title "S(it)"
 set xlabel "it"
 set ylabel "S"
-set yrange[4.2:5.6]
+set autoscale xy
 plot "mulitmesh_S_16.dat" u 1:2 w l lw 2 t 'k=16', "mulitmesh_S_8.dat" u 1:2 w l lw 2 t 'k=8', "mulitmesh_S_4.dat" u 1:2 w l lw 2 t 'k=4', "mulitmesh_S_2.dat" u 1:2 w l lw 2 t 'k=2', "mulitmesh_S_1.dat" u 1:2 w l lw 2 t 'k=1'
 
-# unset logscale x
-# set ylabel 'y'
-# set xlabel 'x'
-
-# # V
-# set view map
-# set title "Relaksacja globalna w=0.6"
-# set xrange [0:15]
-# splot "global_v_0.6.dat" u 1:2:3 w p pt 5 ps 1 palette
-
-# set title "Relaksacja globalna w=1.0"
-# splot "global_v_1.dat" u 1:2:3 w p pt 5 ps 1 palette
-
-# # Error
-# set title "Bład rozwiązania w=0.6"
-# splot "global_v_0.6_error.dat" u 1:2:3 w p pt 5 ps 1 palette
-
-# set title "Bład rozwiązania w=1.0"
-# splot "global_v_1_error.dat" u 1:2:3 w p pt 5 ps 1 palette
 
 unset multiplot
