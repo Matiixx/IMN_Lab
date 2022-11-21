a4e54776023e37ac3b6dc524a079a45ad3b0d001
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-21 15:36:43 +0100

    Adjust plot

--- Lab4/plot.sh
+++ Lab4/plot.sh
@@ -1,18 +1,19 @@
-set term jpeg size 1600,2100
+set term jpeg size 1600,1400
 
 #S(it) global
 set out "./results.jpeg"
-set multiplot layout 3,2
+set multiplot layout 2,2
 set logscale x 10
 
 set title "Relaksacja globalna"
-set ylabel "S"
-set xlabel "nr iteracji"
-plot "global_0.6.dat" u 1:2 w l lw 2 t "w=0.6", "global_1.dat" u 1:2 w l lw 2 t 'w=1.0'
+set ylabel "S(it)"
+set xlabel "it"
+set xrange [1:100000]
+plot "global_s_0.6.dat" u 1:2 w l lw 2 t "it_{max}=36886", "global_s_1.dat" u 1:2 w l lw 2 t 'it_{max}=23096'
 
 #S(it) local
 set title "Relaksacja lokalna"
-plot "local_s_1.dat" u 1:2 w l lw 2 t 'w=1.0', "local_s_1.4.dat" u 1:2 w l lw 2 t 'w=1.4', "local_s_1.8.dat" u 1:2 w l lw 2 t 'w=1.8', "local_s_1.9.dat" u 1:2 w l lw 2 t 'w=1.9'
+plot "local_s_1.dat" u 1:2 w l lw 2 t 'it_{max}=12193', "local_s_1.4.dat" u 1:2 w l lw 2 t 'it_{max}=5547', "local_s_1.8.dat" u 1:2 w l lw 2 t 'it_{max}=1552', "local_s_1.9.dat" u 1:2 w l lw 2 t 'it_{max}=750'
 
 unset logscale x
 set ylabel 'y'
@@ -20,18 +21,18 @@ set xlabel 'x'
 
 # V
 set view map
-set title "Relaksacja globalna w=0.6"
+set title "V(x,y)"
 set xrange [0:15]
 splot "global_v_0.6.dat" u 1:2:3 w p pt 5 ps 1 palette
 
-set title "Relaksacja globalna w=1.0"
-splot "global_v_1.dat" u 1:2:3 w p pt 5 ps 1 palette
+# set title "Relaksacja globalna w=1.0"
+# splot "global_v_1.dat" u 1:2:3 w p pt 5 ps 1 palette
 
 # Error
-set title "Bład rozwiązania w=0.6"
+set title "delta(x,y)"
 splot "global_v_0.6_error.dat" u 1:2:3 w p pt 5 ps 1 palette
 
-set title "Bład rozwiązania w=1.0"
-splot "global_v_1_error.dat" u 1:2:3 w p pt 5 ps 1 palette
+# set title "Bład rozwiązania w=1.0"
+# splot "global_v_1_error.dat" u 1:2:3 w p pt 5 ps 1 palette
 
 unset multiplot
