67e48bf917cf8789c12560f17b26bfe3a15f2faf
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-28 15:33:46 +0100

    Improve plot

--- Lab5/plot.sh
+++ Lab5/plot.sh
@@ -1,40 +1,52 @@
-set term jpeg size 1800,1200
+set term jpeg size 1600,1200
 
 #S(it) global
 set out "./results.jpeg"
 set multiplot layout 2,3
 
+
+#S(it)
+set title "S(it, k)"
+set xlabel "it"
+set ylabel "S"
+set autoscale xy
+plot "multimesh_S_16.dat" u 1:2 w l lw 2 t 'k=16 it=0-80'   ,\
+     "multimesh_S_8.dat"  u 1:2 w l lw 2 t 'k=8 it=81-213'  ,\
+     "multimesh_S_4.dat"  u 1:2 w l lw 2 t 'k=4 it=214-371' ,\
+     "multimesh_S_2.dat"  u 1:2 w l lw 2 t 'k=2 it=372-510' ,\
+     "multimesh_S_1.dat"  u 1:2 w l lw 2 t 'k=1 it=511-516'
+
 # set ylabel "y"
 # set xlabel "x"
-# set view map
+set pm3d
+set view map
+unset surface
 
 # splot "mulitmesh_V_16.dat" u 1:2:3 w p pt 5 ps 1 palette
 set xlabel "x"
 set ylabel "y"
 set zlabel "v"
-set xrange [0:22.4]
-set yrange [0:22.4] 
-# # set logscale z
+set xrange [0:25.6]
+set yrange [0:25.6]
 
-set title "k=16"
-plot [-3:29][-3:29] "mulitmesh_V_16.dat" u 1:2:3 w p pt 5 ps 8 palette notitle
-set title "k=8"
-plot [-3:29][-3:29] "mulitmesh_V_8.dat" u 1:2:3 w p pt 5 ps 4 palette notitle
-set title "k=4"
-plot [-3:29][-3:29] "mulitmesh_V_4.dat" u 1:2:3 w p pt 5 ps 2 palette notitle
-set title "k=2"
-plot [-3:29][-3:29] "mulitmesh_V_2.dat" u 1:2:3 w p pt 5 ps 1 palette notitle
-set title "k=1"
-plot [-3:29][-3:29] "mulitmesh_V_1.dat" u 1:2:3 w p pt 5 ps 0.5 palette notitle
+set palette defined (-1 'blue', 0 'white', 1 'red')
+
+set title "V_{k=16}(x,y)"
+splot "multimesh_V_16.dat" u 1:2:3 w p pt 5 ps 8 notitle
+
+set title "V_{k=8}(x,y)"
+splot "multimesh_V_8.dat" u 1:2:3 w p pt 5 ps 8 notitle
+
+set title "V_{k=4}(x,y)"
+splot "multimesh_V_4.dat" u 1:2:3 w p pt 5 ps 8 notitle
+
+set title "V_{k=2}(x,y)"
+splot "multimesh_V_2.dat" u 1:2:3 w p pt 5 ps 8 notitle
+
+set title "V_{k=1}(x,y)"
+splot "multimesh_V_1.dat" u 1:2:3 w p pt 5 ps 8 notitle
 
 # unset view map
-
-#S(it)
-set title "S(it)"
-set xlabel "it"
-set ylabel "S"
-set autoscale xy
-plot "mulitmesh_S_16.dat" u 1:2 w l lw 2 t 'k=16', "mulitmesh_S_8.dat" u 1:2 w l lw 2 t 'k=8', "mulitmesh_S_4.dat" u 1:2 w l lw 2 t 'k=4', "mulitmesh_S_2.dat" u 1:2 w l lw 2 t 'k=2', "mulitmesh_S_1.dat" u 1:2 w l lw 2 t 'k=1'
 
 
 unset multiplot
