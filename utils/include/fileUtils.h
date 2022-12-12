7016bf56cecddef4389a6939f1388e7004606d64
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-12-12 20:54:42 +0100

    Save to file with initializer list

--- utils/include/fileUtils.h
+++ utils/include/fileUtils.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <initializer_list>
 
 void clearFile(const std::string &);
 void saveToFile(const std::string &, const double &, const double &);
@@ -11,3 +12,16 @@ void saveToFile(const std::string &, const double &, const double &, const doubl
 void saveToFile(const std::string &, const double &, const double &, const double &, const double &, const double &, const double &);
 void saveVector2DToFile(const std::string &, std::vector<std::vector<double>> &, const int &, const int &, const double &, const int &);
 void addEndLineToFile(const std::string &f);
+
+template <typename T>
+void saveToFile(const std::string &filename, std::initializer_list<T> list)
+{
+  std::fstream file;
+  file.open(filename, std::ios::app);
+  for (const auto el : list)
+  {
+    file << el << " ";
+  }
+  file << "\n";
+  file.close();
+}
